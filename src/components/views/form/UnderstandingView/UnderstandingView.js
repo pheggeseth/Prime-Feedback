99e7794c58129a6f617e888803af9bc92bee860b
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-07 21:02:03 -0500

    updated view

--- src/components/views/form/UnderstandingView/UnderstandingView.js
+++ src/components/views/form/UnderstandingView/UnderstandingView.js
@@ -1,40 +1,19 @@
 import React, { Component } from 'react';
 import FormTemplate from '../../../FormTemplate/FormTemplate.js';
-import { connect } from 'react-redux';
-import { UPDATE_UNDERSTANDING } from '../../../../redux/actions.js';
 
 class UnderstandingView extends Component {
-  saveUnderstandingToRedux = value => {
-    const action = {
-      type: UPDATE_UNDERSTANDING,
-      payload: value
-    };
-    this.props.dispatch(action);
-  };
-
-  goToPage = path => this.props.history.push(path);
-
-  handleBack = () => {
-    this.goToPage('/form/feeling');
-  };
-
-  handleNext = value => {
-    this.saveUnderstandingToRedux(value);
-    this.goToPage('/form/support');
-  };
-
   render() {
     return(
       <div>
         <FormTemplate 
           category="understanding" 
           prompt="How well did you understand today's content?" 
-          onBack={this.handleBack}
-          onNext={this.handleNext} />
+          history={this.props.history}
+          prevPage="/form/feeling"
+          nextPage="/form/support" />
       </div>
     );
   }
 }
 
-const mapReduxStateToProps = (reduxState) => ({reduxState});
-export default connect(mapReduxStateToProps)(UnderstandingView);
+export default UnderstandingView;
