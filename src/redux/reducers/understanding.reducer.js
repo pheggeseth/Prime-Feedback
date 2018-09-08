e5a7a3f9f213cf42209bcc9d6a6fc4558428046e
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-07 20:31:08 -0500

    updated understanding reducer

--- src/redux/reducers/understanding.reducer.js
+++ src/redux/reducers/understanding.reducer.js
@@ -1,5 +1,12 @@
+import { UPDATE_UNDERSTANDING } from '../actions.js';
+
 const understanding = (state = '', action) => {
-  return state;
+  if (action.type === UPDATE_UNDERSTANDING) {
+    const newState = action.payload;
+    return newState;
+  } else {
+    return state;
+  }
 };
 
 export default understanding;
