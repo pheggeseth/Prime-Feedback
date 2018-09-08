5b76ff915ecc5c6eb68942056fa97525fba928a5
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-07 21:11:09 -0500

    updated views

--- src/components/views/form/CommentsView/CommentsView.js
+++ src/components/views/form/CommentsView/CommentsView.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import FormTemplate from '../../../FormTemplate/FormTemplate.js';
 
 class CommentsView extends Component {
   render() {
     return(
       <div>
-        CommentsView
+        <FormTemplate
+          category="comments"
+          prompt="Any comments you would like to leave?"
+          history={this.props.history}
+          prevPage="/form/support"
+          nextPage="/form/success" />
       </div>
     );
   }
--- src/components/views/form/SupportView/SupportView.js
+++ src/components/views/form/SupportView/SupportView.js
@@ -6,11 +6,11 @@ class SupportView extends Component {
     return(
       <div>
         <FormTemplate
-        category="support"
-        prompt="How well are you being supported?"
-        history={this.props.history}
-        prevPage="/form/understanding"
-        nextPage="/form/comments" />
+          category="support"
+          prompt="How well are you being supported?"
+          history={this.props.history}
+          prevPage="/form/understanding"
+          nextPage="/form/comments" />
       </div>
     );
   }
