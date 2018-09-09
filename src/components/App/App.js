fb2c4b34bae34db8721b97cfbf03327d68a84cbd
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-09 14:26:22 -0500

    added Switch to Router to allow all non-specified paths to redirect to the beginning of the form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Header from '../Header/Header.js';
 import FeelingView from '../views/form/FeelingView/FeelingView.js';
 import UnderstandingView from '../views/form/UnderstandingView/UnderstandingView.js';
@@ -20,14 +20,19 @@ class App extends Component {
       <Router>
         <div className="App">
           <Header />
-          <Route exact path="/" component={() => <Redirect to="/form/feeling"/>} />
-          <Route path="/form/feeling" component={FeelingView} />
-          <Route path="/form/understanding" component={UnderstandingView} />
-          <Route path="/form/support" component={SupportView} />
-          <Route path="/form/comments" component={CommentsView} />
-          <Route path="/form/submit" component={SubmitView} />
-          <Route path="/form/success" component={SuccessView} />
-          <Route path="/admin" component={AdminView} />
+          {/* using a Switch allows only one Route component to render at a time */}
+          <Switch> 
+            <Route path="/form/feeling" component={FeelingView} />
+            <Route path="/form/understanding" component={UnderstandingView} />
+            <Route path="/form/support" component={SupportView} />
+            <Route path="/form/comments" component={CommentsView} />
+            <Route path="/form/submit" component={SubmitView} />
+            <Route path="/form/success" component={SuccessView} />
+            <Route path="/admin" component={AdminView} />
+            {/* all other routes will automatically redirect to the beginning of the form,
+            as this final route doesn't have a path specified */}
+            <Route component={() => <Redirect to="/form/feeling"/>} /> 
+          </Switch>
         </div>
       </Router>
     );
