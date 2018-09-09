41b5b48aeff0870815a989160566bb8c2f2a7b76
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-09 13:09:05 -0500

    upgraded AdminView to Material-UI

--- src/components/views/AdminView/AdminView.js
+++ src/components/views/AdminView/AdminView.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import moment from 'moment';
 
 class AdminView extends Component {
   constructor(props) {
@@ -53,36 +64,75 @@ class AdminView extends Component {
 
   render() {
     return (
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Feeling</th>
-              <th>Understanding</th>
-              <th>Support</th>
-              <th>Comments</th>
-              <th>Flagged</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.feedback.map(entry => (
-              <tr key={entry.id}>
-                <td>{entry.feeling}</td>
-                <td>{entry.understanding}</td>
-                <td>{entry.support}</td>
-                <td>{entry.comments}</td>
-                <td><input type="checkbox" checked={entry.flagged} onChange={this.toggleFeedbackFlag(entry)}/>{`${entry.flagged}`}</td>
-                <td><button onClick={this.deleteFeedback(entry.id)}>Delete</button></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <Grid container justify="center">
+        <Grid item sm={10}>
+          <Paper style={{marginTop: '40px', padding: '20px'}}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Feedback Date</TableCell>
+                  <TableCell numeric>Feeling</TableCell>
+                  <TableCell numeric>Understanding</TableCell>
+                  <TableCell numeric>Support</TableCell>
+                  <TableCell>Comments</TableCell>
+                  <TableCell>Flagged</TableCell>
+                  <TableCell>Delete</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.feedback.map(entry => (
+                  <TableRow key={entry.id}>
+                    <TableCell>{moment(entry.date).format('M/D/YYYY, h:mmA')}</TableCell>
+                    <TableCell numeric>{entry.feeling}</TableCell>
+                    <TableCell numeric>{entry.understanding}</TableCell>
+                    <TableCell numeric>{entry.support}</TableCell>
+                    <TableCell>{entry.comments}</TableCell>
+                    <TableCell>
+                      <Checkbox 
+                        checked={entry.flagged}
+                        onChange={this.toggleFeedbackFlag(entry)}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Button color="secondary" onClick={this.deleteFeedback(entry.id)}>
+                        <DeleteForeverIcon />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Paper>
+        </Grid>
+      </Grid>
+      // <div>
+      //   <table>
+      //     <thead>
+      //       <tr>
+      //         <th>Feeling</th>
+      //         <th>Understanding</th>
+      //         <th>Support</th>
+      //         <th>Comments</th>
+      //         <th>Flagged</th>
+      //         <th>Delete</th>
+      //       </tr>
+      //     </thead>
+      //     <tbody>
+      //       {this.state.feedback.map(entry => (
+      //         <tr key={entry.id}>
+      //           <td>{entry.feeling}</td>
+      //           <td>{entry.understanding}</td>
+      //           <td>{entry.support}</td>
+      //           <td>{entry.comments}</td>
+      //           <td><input type="checkbox" checked={entry.flagged} onChange={this.toggleFeedbackFlag(entry)}/>{`${entry.flagged}`}</td>
+      //           <td><button onClick={this.deleteFeedback(entry.id)}>Delete</button></td>
+      //         </tr>
+      //       ))}
+      //     </tbody>
+      //   </table>
+      // </div>
     );
   }
 }
 
-// const mapReduxStateToProps = reduxState => ({reduxState});
-// export default connect(mapReduxStateToProps)(AdminView);
 export default AdminView;
