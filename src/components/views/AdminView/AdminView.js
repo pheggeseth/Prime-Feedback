cd0df4e53afb6223970410c528509a1c32e6c9cb
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-09 13:52:20 -0500

    removed connect import

--- src/components/views/AdminView/AdminView.js
+++ src/components/views/AdminView/AdminView.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
