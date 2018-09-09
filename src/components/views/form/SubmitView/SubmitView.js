0aa71e5d811e03dc562127b68c3e66fec45aa2d8
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-09 09:16:50 -0500

    set up basic SumbitView with Material-UI

--- src/components/views/form/SubmitView/SubmitView.js
+++ src/components/views/form/SubmitView/SubmitView.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 class SubmitView extends Component {
   // uses react-router-dom to navigate to a specific view
@@ -31,10 +35,37 @@ class SubmitView extends Component {
 
   render() {
     return(
-      <div>
-        <h1>Would you like to submit your feedback?</h1>
-        <button type="button" onClick={this.handleSubmit}>Submit Feedback</button>
-      </div>
+      <Grid container justify="center">
+        <Grid item sm={6}>
+          <Paper style={{marginTop: '40px', padding: '20px'}}>
+            <Grid container justify="center" alignItems="center" style={{height: '60%'}}>
+              <Grid item>
+              <Typography variant="display1" gutterBottom>
+                Would you like to submit your feedback?
+              </Typography>
+              </Grid>
+            </Grid>
+            <Grid container justify="space-between" alignItems="flex-end" style={{height: '40%'}}>
+              <Grid item style={{flexGrow: 1}}>
+                <Grid container justify="flex-start">
+                  <Button color="secondary">Start over</Button>
+                </Grid>
+              </Grid>
+              <Grid item style={{marginRight: '10px'}}>
+                <Button>Back</Button>
+              </Grid>
+              <Grid item>
+                <Button color="primary" variant="raised">Submit</Button>
+              </Grid>
+              
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+      // <div>
+      //   <h1>Would you like to submit your feedback?</h1>
+      //   <button type="button" onClick={this.handleSubmit}>Submit Feedback</button>
+      // </div>
     );
   }
 }
